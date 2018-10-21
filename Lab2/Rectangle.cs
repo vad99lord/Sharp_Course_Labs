13a065df003cfcf84682b6470f824ee93e18e872
Vadim <vad99lord@gmail.com>
2018-10-21 18:26:26 +0300

    Add project files.

---
+++ Lab2/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab2
+{
+    public class Rectangle : GeometricFigure
+    {
+        private double a, b;
+        public Rectangle(double a, double b) {
+            A = a;
+            B = b;
+        }
+
+        public double A { get => a; set => a = value; }
+        public double B { get => b; set => b = value; }
+
+        public override double calcArea()
+        {
+            return A * B;
+        }
+        public override string ToString()
+        {
+            return "Rectangle, a = " + A + ",b = " + B+",area = " + calcArea();
+        }
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+}
